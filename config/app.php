690465ea3f26c4b88a0f80ae57c0df3d4c641a97
PabloProgramador77 <pabloprogramador77@gmail.com>
2023-12-19 00:53:37 -0600

    Spatie Recien Instalado y Configurado

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Spatie Roles & Permissions User's
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ])->toArray(),
 
     /*
---
+++ resources/views/cliente/index.blade.php
@@ -0,0 +1,55 @@
+@extends('home')
+@section('contenido')
+    <div class="container-fluid col-md-12 p-2 bg-white">
+
+        <div class="container-fluid row col-md-12 border-bottom p-2">
+
+            <div class="col-md-7">
+                <h4 class="my-auto"><i class="fas fa-smile"></i> Clientes de Restaurante</h4>
+                <p class="fs-6 fw-semibold text-secondary"><i class="fas fa-user-shield"></i> Panel de Administrador</p>
+            </div>
+
+            @php
+                $heads = [
+
+                    'Usuario', 'Email', 'Acciones'
+
+                ];
+            @endphp
+            
+            <div class="container-fluid col-md-12 my-3">
+                <x-adminlte-datatable id="salsas" :heads="$heads" theme="light" striped hoverable bordered compressed beautify>
+                    
+                    @if( count( $clientes ) > 0 )
+                        @foreach($clientes as $cliente)
+                            <tr>
+                                <td>{{ $cliente->name }}</td>
+                                <td>{{ $cliente->email }}</td>
+                                <td>
+                                    <x-adminlte-button class="editar" id="editar" label="Editar" theme="info" data-toggle="modal" data-target="#modalEditar" data-id="{{ $cliente->id }}" icon="fas fa-pen"></x-adminlte-button>
+                                    <x-adminlte-button class="eliminar" id="eliminar" label="Borrar" theme="danger" data-id="{{ $cliente->id }}" icon="fas fa-trash-alt"></x-adminlte-button>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    @else
+                        <tr>
+                            <td colspan="4" class="text-info">Sin clientes registrados</td>
+                        </tr>
+                    @endif
+                    
+                </x-adminlte-datatable>
+            </div>
+
+        </div>
+
+    </div>
+
+    <script src="{{ asset('jquery-3.7.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('sweetAlert.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/cliente/agregar.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/cliente/buscar.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/cliente/actualizar.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/cliente/borrar.js') }}" type="text/javascript"></script>
+
+@stop
